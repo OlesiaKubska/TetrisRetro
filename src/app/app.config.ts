3fbe6c388068963862a90917192a045d3c8e26ca
OlesiaKubska <kublesia0908@gmail.com>
2024-05-11 12:34:36 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { IntroPageComponent } from './intro-page/intro-page.component';
+import { GamePageComponent } from './game-page/game-page.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration()]
+  providers: [
+    provideRouter([
+      { path: 'intro', component: IntroPageComponent },
+      { path: 'game', component: GamePageComponent },
+    ]),
+    provideHttpClient(),
+  ],
 };
