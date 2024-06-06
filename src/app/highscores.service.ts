e11043a222efaea649c3f5723b947f0ec3c5ded6
OlesiaKubska <kublesia0908@gmail.com>
2024-06-06 13:30:41 +0200

    update HighscoresService to fetch and sort game scores dynamically

--- src/app/highscores.service.ts
+++ src/app/highscores.service.ts
@@ -3,15 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+interface Highscore {
+  name: string;
+  score: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class HighscoresService {
-  constructor(private http: HttpClient) {}
+  private baseUrl = 'https://scores.chrum.it/scores';
 
-  getHighscores(game: string): Observable<any[]> {
-    return this.http
-      .get<any[]>(`/scores/${game}`)
+  constructor(private _http: HttpClient) {}
+
+  getHighscores(game: string): Observable<Highscore[]> {
+    return this._http
+      .get<Highscore[]>(`${this.baseUrl}/${game}`)
       .pipe(
         map((response) =>
           response.sort((a, b) => b.score - a.score).slice(0, 10)
